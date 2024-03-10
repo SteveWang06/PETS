11a3c2696c375952bc6bcbe507175ad7dbcf44a0
Nemean0724 <91184054+Nemean0724@users.noreply.github.com>
2024-03-10 12:13:38 +0800

    Update PostRepository.java

--- server/pet/src/main/java/com/project/pet/repository/PostRepository.java
+++ server/pet/src/main/java/com/project/pet/repository/PostRepository.java
@@ -4,11 +4,6 @@ import com.project.pet.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Override
-public void updatePost(Post updatedPost) {
-    postRepository.save(updatedPost);
-}
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
