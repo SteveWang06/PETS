60652325dd63c817a45a2612f78b6e808eb4bc59
Nemean0724 <91184054+Nemean0724@users.noreply.github.com>
2024-03-10 12:08:38 +0800

    Update PostImageRepository.java

--- server/pet/src/main/java/com/project/pet/repository/PostImageRepository.java
+++ server/pet/src/main/java/com/project/pet/repository/PostImageRepository.java
@@ -3,5 +3,7 @@ package com.project.pet.repository;
 import com.project.pet.models.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+public void updatePost(Post updatedPost);
+
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 }
