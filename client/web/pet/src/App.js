de00cbfd74553ca0e104c38e0f63a8f5ce09698d
chu-yin <91184619+chu-yin@users.noreply.github.com>
2024-04-01 10:03:42 +0800

    Update App.js

--- client/web/pet/src/App.js
+++ client/web/pet/src/App.js
@@ -1,44 +1,15 @@
-import './App.css';
-import logo from './petlogo.png';
-//import search from './search.png';
+// import Dashboard from "./pages/Dashboard";
+// import User from "./pages/User";
+import Contain from "./components/ContainerDashboard"
+import Footer from "./components/Footer"
 
-function Web(){
-  return(
-    <div className="Web">
-      <a href="#home">
-        <img src={logo} className="Web-logo" alt="logo" />
-      </a>
-      <input type="checkbox"  id="sideMenu-active" />
-      <div className="sideMenu">
-        <form>
-        {/* <img src={search} className="search-icon" alt="search"/> */}
-            <input class="search-box" type="search" placeholder="Search"/>
-        </form>
-        <ul className="menu">
-          <li><a className="home" href="#home">Home</a></li>
-          <li><a className="shop" href="#shop">Shop</a></li>
-        <li><a className="hospital" href="#hospital">Hospital</a></li>
-        <li>
-          <a className="pets" href="#pets">Pets</a>
-          <ul>
-            <li><a href="#cats">Cats</a></li>
-            <li><a href="#dogs">Dogs</a></li>
-            <li><a href="#other">Other</a></li>
-          </ul>
-        </li>
-        <li><a className="profile" href="#profile">Profile</a></li>
-        <li><a className="Notifications" href="#notifications">Notifications</a></li>
-        </ul>
-        <a className="setting" href="#setting">Setting</a>
-        <a className="login" href="#login">Login</a>
-        <label for="sideMenu-active">
-          <i class="fas fa-angle-right"></i>
-        </label>
-      </div>
-      <select name="language">
-      <option value="English">English</option>
-      </select>
+
+function App() {
+  return (
+    <div>
+      <Contain/>
+      <Footer/>
     </div>
   );
 }
-export default Web;
+export default App;
