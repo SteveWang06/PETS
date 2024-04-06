68461158138bf8634737ee864d8a2535822825dd
nguyenthanhphuoc <d1072770@o365.fcu.edu.tw>
2024-04-06 15:35:46 +0800

    render image on home screen

--- server/pet/src/main/java/com/project/pet/controller/ImageController.java
+++ server/pet/src/main/java/com/project/pet/controller/ImageController.java
@@ -1,11 +1,9 @@
 package com.project.pet.controller;
 
-<<<<<<< HEAD
 import com.project.pet.models.Image;
 import com.project.pet.services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-=======
 
 import com.project.pet.models.FileData;
 import com.project.pet.models.Post;
@@ -17,12 +15,10 @@ import com.project.pet.services.PostServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
->>>>>>> origin/master
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-<<<<<<< HEAD
 import org.springframework.http.HttpHeaders;
 
 import java.io.IOException;
@@ -75,39 +71,6 @@ public class ImageController {
       // Trả về lỗi 404 Not Found nếu không tìm thấy hình ảnh
       return ResponseEntity.notFound().build();
     }
-=======
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("api/auth")
-public class ImageController {
-
-
-  @Autowired
-  FilesStorageService storageService;
-
-  @PostMapping("/image")
-  public ResponseEntity<?> uploadImageToFIleSystem(@RequestParam("image")MultipartFile file) throws IOException {
-    String uploadImage = storageService.uploadImageToFileSystem(file);
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(uploadImage);
-  }
-
-  @GetMapping("/image/{fileName}")
-  public ResponseEntity<?> downloadImageFromFileSystem(@PathVariable String fileName) throws IOException {
-    byte[] imageData=storageService.downloadImageFromFileSystem(fileName);
-    return ResponseEntity.status(HttpStatus.OK)
-        .contentType(MediaType.valueOf("image/png"))
-        .body(imageData);
-
->>>>>>> origin/master
   }
 }
