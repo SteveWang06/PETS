a6763d37bd2a1a80c531beb657ee345d773424cd
nguyenthanhphuoc <d1072770@o365.fcu.edu.tw>
2024-04-07 21:34:57 +0800

    done function of home screen

--- server/pet/src/main/java/com/project/pet/dto/SignupDTO.java
+++ server/pet/src/main/java/com/project/pet/dto/SignupDTO.java
@@ -1,5 +1,6 @@
 package com.project.pet.dto;
 
+import com.project.pet.models.Image;
 import com.project.pet.models.Role;
 import lombok.Data;
 
@@ -13,5 +14,32 @@ public class SignupDTO {
   private String email;
 
   private String password;
+
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
 
 }
--- server/pet/src/main/java/com/project/pet/services/AuthServiceImpl.java
+++ server/pet/src/main/java/com/project/pet/services/AuthServiceImpl.java
@@ -3,21 +3,29 @@ package com.project.pet.services;
 import com.project.pet.dto.LoginDto;
 import com.project.pet.dto.SignupDTO;
 import com.project.pet.dto.UserDTO;
+import com.project.pet.models.Image;
 import com.project.pet.models.Role;
 import com.project.pet.models.RoleEnum;
 import com.project.pet.models.User;
 import com.project.pet.repository.RoleRepository;
 import com.project.pet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -28,6 +36,8 @@ public class AuthServiceImpl implements AuthService{
   private final PasswordEncoder passwordEncoder;
   private final AuthenticationManager authenticationManager;
 
+  @Value("${avatar.upload.dir}")
+  private String uploadDir;
   public AuthServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
     this.userRepository = userRepository;
     this.roleRepository = roleRepository;
@@ -35,21 +45,55 @@ public class AuthServiceImpl implements AuthService{
     this.authenticationManager = authenticationManager;
   }
 
-  @Override
-  public User createUser(SignupDTO input) {
+
+  public User createUser(String username, String email, String password, MultipartFile images) throws IOException {
     Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.USER);
 
     if (optionalRole.isEmpty()) {
       return null;
     }
 
-    var user = new User()
-        .setUserName(input.getUsername())
-        .setEmail(input.getEmail())
-        .setPassword(passwordEncoder.encode(input.getPassword()))
-        .setRole(optionalRole.get());
+    ImageInfo avatarUrl = saveImage(images);
+    Image savedImage = new Image();
+    savedImage.setImageUrl(avatarUrl.getUniqueFileName());
+    savedImage.setImagePath(String.valueOf(avatarUrl.getFilePath()));
+    User user = new User();
+    user.setUserName(user.getUsername());
+    user.setEmail(user.getEmail());
+    user.setAvatar(savedImage);
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    user.setRole(optionalRole.get());
     return userRepository.save(user);
 
+  }
+
+  public class ImageInfo {
+    private String uniqueFileName;
+    private Path filePath;
+
+    public ImageInfo(String uniqueFileName, Path filePath) {
+      this.uniqueFileName = uniqueFileName;
+      this.filePath = filePath;
+    }
+
+    public String getUniqueFileName() {
+      return uniqueFileName;
+    }
+
+    public Path getFilePath() {
+      return filePath;
+    }
+  }
+
+  private ImageInfo saveImage(MultipartFile file) throws IOException {
+    // Tạo tên duy nhất cho hình ảnh
+    String uniqueFileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+    // Tạo đường dẫn đầy đủ của hình ảnh
+    Path filePath = Paths.get(uploadDir + uniqueFileName);
+    // Lưu trữ hình ảnh vào đường dẫn
+    Files.copy(file.getInputStream(), filePath);
+    // Trả về thông tin hình ảnh
+    return new ImageInfo(uniqueFileName, filePath);
   }
 
   public User authenticate(LoginDto input) {
