d50e5d9f55849f83e1e1005b11625281b14b5dd0
SteveWang06 <thanhphuoc1306vn@gmail.com>
2024-05-27 12:23:00 +0800

    take back file

---
+++ server/pet/src/main/java/com/project/pet/controller/CommentController.java
@@ -0,0 +1,54 @@
+package com.project.pet.controller;
+
+
+import com.project.pet.models.Post;
+import com.project.pet.models.PostComment;
+import com.project.pet.models.User;
+import com.project.pet.repository.PostRepository;
+import com.project.pet.services.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("api/post/comment")
+public class CommentController {
+
+  private final CommentService commentService;
+
+  @Autowired
+  private PostRepository postRepository;
+  @Autowired
+  public CommentController(CommentService commentService) {
+    this.commentService = commentService;
+  }
+
+  @PostMapping("{postId}/create")
+  public ResponseEntity<PostComment> createComment(@PathVariable Long postId, @RequestParam User userId, @RequestParam String content) {
+    Post post = postRepository.findById(postId).orElse(null);
+
+    // Kiểm tra xem post có tồn tại không
+    if (userId == null || content == null) {
+      return ResponseEntity.badRequest().build();
+    }
+
+    PostComment comment = commentService.createComment(postId, userId, content);
+    return new ResponseEntity<>(comment, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{commentId}")
+  public ResponseEntity<PostComment> updateComment(@PathVariable Long commentId, @RequestParam String content) {
+    PostComment updated = commentService.updateComment(commentId, content);
+    return ResponseEntity.ok(updated);
+  }
+
+  @DeleteMapping("/{commentId}")
+  public ResponseEntity<String> deleteComment(@PathVariable Long commentId) {
+    commentService.deleteComment(commentId);
+    return ResponseEntity.ok("Comment deleted successfully");
+  }
+
+
+}
