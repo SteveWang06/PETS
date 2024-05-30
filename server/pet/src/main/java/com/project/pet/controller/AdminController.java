4b3f336bc67f5d1813a3ba27c1b84997c37d9d05
SteveWang06 <thanhphuoc1306vn@gmail.com>
2024-05-30 16:06:32 +0800

    add description for api on swagger

--- server/pet/src/main/java/com/project/pet/controller/AdminController.java
+++ server/pet/src/main/java/com/project/pet/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.project.pet.controller;
 import com.project.pet.dto.SignupDTO;
 import com.project.pet.models.User;
 import com.project.pet.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/auth")
 @RestController
 public class AdminController {
+
   private final UserService userService;
 
   public AdminController(UserService userService) {
     this.userService = userService;
   }
 
+  @Operation(summary = "Create administrator", description = "Create a new administrator user")
+  @ApiResponse(responseCode = "200", description = "Administrator created successfully")
   @PostMapping("/admins")
   @PreAuthorize("hasRole('SUPER_ADMIN')")
   @SecurityRequirement(name = "bearerAuth")
